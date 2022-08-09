1e2ea144feb98b6862c288fc3628fa96c09e3bb9
RafaelVN <contato@RafaelVN.com>
2022-08-09 14:08:16 -0300

    beecrowd_1007

---
+++ src/program/Main.java
@@ -0,0 +1,33 @@
+package program;
+
+/*
+beecrowd | 1007 - Diferença
+
+Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença
+do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+
+Entrada
+O arquivo de entrada contém 4 valores inteiros.
+
+Saída
+Imprima a mensagem DIFERENCA com todas as letras maiúsculas, conforme exemplo abaixo,
+com um espaço em branco antes e depois da igualdade.
+ */
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		int D = sc.nextInt();
+		int DIFERENCA = A * B - C * D;
+		
+		System.out.println("DIFERENCA = " + DIFERENCA);
+		sc.close();
+	}
+}
